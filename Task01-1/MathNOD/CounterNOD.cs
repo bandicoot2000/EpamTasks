4b2f579fe477d9ed51a7ab10aeb50c0612a9dc87
bandicoot2000 <44677038+bandicoot2000@users.noreply.github.com>
2020-07-12 14:25:32 +0300

    Created method GetNOD and unit tests passed .

--- Task01-1/MathNOD/CounterNOD.cs
+++ Task01-1/MathNOD/CounterNOD.cs
@@ -8,5 +8,43 @@ namespace MathNOD
 {
     public class CounterNOD
     {
+        private int numberFirst;
+
+        public int NumberFirst 
+        {
+            get { return numberFirst; } 
+            set { numberFirst = value > 0 ? value : 1; } 
+        }
+
+        private int numberSecond;
+
+        public int NumberSecond 
+        {
+            get { return numberSecond; }
+            set { numberSecond = value > 0 ? value : 1; }
+        }
+
+        public CounterNOD(int numberFirst, int numberSecond)
+        {
+            NumberFirst = numberFirst;
+            NumberSecond = numberSecond;
+        }
+
+        public int GetNOD()
+        {
+            int numberFirst = NumberFirst;
+            int numberSecond = NumberSecond;
+            int numberTemp = 0;
+
+            while(numberSecond != 0)
+            {
+                numberTemp = numberSecond;
+                numberSecond = numberFirst % numberSecond;
+                numberFirst = numberTemp;
+            }
+
+            return numberFirst;
+        }
+
     }
 }
---
+++ Task01-1/UnitTestMathNOD/CounterNODTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using MathNOD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestMathNOD
+{
+    [TestClass]
+    public class CounterNODTest
+    {
+
+        private static int GCD(int numberFirst, int numberSecond)
+        {
+            if(numberFirst == 0)
+            {
+                return numberSecond;
+            }
+            else
+            {
+                int min = Math.Min(numberFirst, numberSecond);
+                int max = Math.Max(numberFirst, numberSecond);
+                return GCD(max % min, min);
+            }
+        }
+
+
+        [TestMethod]
+        public void GetNODTest()
+        {
+            CounterNOD counterNOD = new CounterNOD(1, 1);
+            for (int numberFirst = 1; numberFirst < 1000; numberFirst++)
+            {
+                for (int numberSecond = 1; numberSecond < 1000; numberSecond++)
+                {
+                    counterNOD.NumberFirst = numberFirst;
+                    counterNOD.NumberSecond = numberSecond;
+                    Assert.AreEqual(GCD(numberFirst, numberSecond), counterNOD.GetNOD());
+                }
+            }
+        }
+    }
+}
