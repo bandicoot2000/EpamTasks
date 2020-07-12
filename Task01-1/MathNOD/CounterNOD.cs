02b56ad640eaf2b4ee8acab005b17b8dcabc5407
bandicoot2000 <44677038+bandicoot2000@users.noreply.github.com>
2020-07-12 18:08:32 +0300

    Add xml documentation.

--- Task01-1/MathNOD/CounterNOD.cs
+++ Task01-1/MathNOD/CounterNOD.cs
@@ -7,10 +7,15 @@ using System.Threading.Tasks;
 
 namespace MathNOD
 {
+    /// <summary>
+    /// Class allowing to calculate NOD.
+    /// </summary>
     public class CounterNOD
     {
         private uint numberFirst;
-
+        /// <summary>
+        /// First number to calculate the NOD.
+        /// </summary>
         public uint NumberFirst 
         {
             get { return numberFirst; } 
@@ -19,7 +24,9 @@ namespace MathNOD
         }
 
         private uint numberSecond;
-
+        /// <summary>
+        /// Second number to calculate the NOD.
+        /// </summary>
         public uint NumberSecond 
         {
             get { return numberSecond; }
@@ -27,33 +34,64 @@ namespace MathNOD
                     throw new Exception("Zero exception, number must be greater than zero"); }
         }
 
-
+        /// <summary>
+        /// Class constructor CounterNOD.
+        /// </summary>
+        /// <param name="numberFirst">First number to calculate the NOD.</param>
+        /// <param name="numberSecond">Second number to calculate the NOD.</param>
         public CounterNOD(uint numberFirst, uint numberSecond)
         {
             NumberFirst = numberFirst;
             NumberSecond = numberSecond;
         }
 
+        /// <summary>
+        /// Calculate NOD of two numbers Euclid's algorithm.
+        /// </summary>
+        /// <returns>NOD of two numbers.</returns>
         public uint GetNOD()
         {
             return NOD(numberFirst, numberSecond);
         }
 
+        /// <summary>
+        /// Calculate NOD of three numbers Euclid's algorithm.
+        /// </summary>
+        /// <param name="numberThird">Third number to calculate the NOD.</param>
+        /// <returns>NOD of three numbers.</returns>
         public uint GetNOD(uint numberThird)
         {
             return NOD(GetNOD(), numberThird);
         }
 
+        /// <summary>
+        /// Calculate NOD of four numbers Euclid's algorithm.
+        /// </summary>
+        /// <param name="numberThird">Third number to calculate the NOD.</param>
+        /// <param name="numberFourth">Fourth number to calculate the NOD.</param>
+        /// <returns>NOD of four numbers.</returns>
         public uint GetNOD(uint numberThird, uint numberFourth)
         {
             return NOD(GetNOD(numberThird), numberFourth);
         }
 
+        /// <summary>
+        /// Calculate NOD of five numbers Euclid's algorithm.
+        /// </summary>
+        /// <param name="numberThird">Third number to calculate the NOD.</param>
+        /// <param name="numberFourth">Fourth number to calculate the NOD.</param>
+        /// <param name="numberFifth">Fifth number to calculate the NOD.</param>
+        /// <returns>NOD of five numbers.</returns>
         public uint GetNOD(uint numberThird, uint numberFourth, uint numberFifth)
         {
             return NOD(GetNOD(numberThird, numberFourth), numberFifth);
         }
 
+        /// <summary>
+        /// Calculate NOD of two numbers Euclid's algorithm.
+        /// </summary>
+        /// <param name="time">NOD calculation time.</param>
+        /// <returns>NOD of two numbers.</returns>
         public uint GetNOD(out long time)
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
@@ -63,8 +101,11 @@ namespace MathNOD
             return answer;
         }
 
-
-
+        /// <summary>
+        /// Calculate NOD of two numbers Stein's algorithm.
+        /// </summary>
+        /// <param name="time">NOD calculation time.</param>
+        /// <returns>NOD of two numbers.</returns>
         public uint GetNODStein(out long time)
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
@@ -74,6 +115,29 @@ namespace MathNOD
             return answer;
         }
 
+        /// <summary>
+        /// Collects data on the time of calculation of NOD.
+        /// </summary>
+        /// <param name="numberFirst">First number to calculate the NOD.</param>
+        /// <param name="nemberSecondMax">The largest second number to calculate the NOD.</param>
+        /// <returns>NOD calculation time data.</returns>
+        public static long[,] GetTimeData(uint numberFirst, uint nemberSecondMax = 10000)
+        {
+            long[,] answer = new long[2, nemberSecondMax];
+            long time;
+            CounterNOD counterNOD = new CounterNOD(numberFirst, 1);
+            for (uint number = 1; number < nemberSecondMax; number++)
+            {
+                counterNOD.numberSecond = number;
+
+                counterNOD.GetNOD(out time);
+                answer[0, number - 1] = time;
+
+                counterNOD.GetNODStein(out time);
+                answer[1, number - 1] = time;
+            }
+            return answer;
+        }
 
         private uint NOD(uint numberFirst, uint numberSecond)
         {
