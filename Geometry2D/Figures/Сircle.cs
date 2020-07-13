2d30392d087b78e5f28f17b0a72bdda799880581
bandicoot2000 <44677038+bandicoot2000@users.noreply.github.com>
2020-07-13 20:14:25 +0300

    Created Geometry lib for task01 - 2.

---
+++ Geometry2D/Figures/Сircle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry2D
+{
+    public class Сircle : Figure
+    {
+        protected double radius;
+
+        public Сircle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Сircle сircle &&
+                   radius == сircle.radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public override int GetHashCode()
+        {
+            return -457394261 + radius.GetHashCode();
+        }
+
+        public override double GetPerimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public override string ToString()
+        {
+            return "Circle " + radius;
+        }
+    }
+}
