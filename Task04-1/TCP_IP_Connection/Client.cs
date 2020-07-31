5a4213ccab8e9cac8497b04a660a057eec1a9054
bandicoot2000 <44677038+bandicoot2000@users.noreply.github.com>
2020-07-31 15:14:18 +0300

    Start work

---
+++ Task04-1/TCP_IP_Connection/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_IP_Connection
+{
+    public class Client
+    {
+        private TcpClient tcpClient;
+
+
+        public delegate void MassageHandler(string massage);
+        public event MassageHandler MassageEvent; 
+
+        public Client(string server, int port)
+        {
+            tcpClient = new TcpClient();
+            tcpClient.Connect(server, port);
+        }
+
+        public void ReadMassage()
+        {
+            byte[] data = new byte[256];
+            StringBuilder massage = new StringBuilder();
+            NetworkStream stream = tcpClient.GetStream();
+
+            do
+            {
+                int bytes = stream.Read(data, 0, data.Length);
+                massage.Append(Encoding.UTF8.GetString(data, 0, bytes));
+            }
+            while (stream.DataAvailable); 
+
+            stream.Close();
+        }
+
+        public void WriteMassage(string massage)
+        {
+            NetworkStream stream = tcpClient.GetStream();
+            byte[] data = Encoding.UTF8.GetBytes(massage);
+            stream.Write(data, 0, data.Length);
+            stream.Close();
+        }
+
+        public void Close()
+        {
+            tcpClient.Close();
+        }
+    }
+}
---
+++ Task04-1/TCP_IP_Connection/Server.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_IP_Connection
+{
+    public class Server
+    {
+        private TcpListener tcpServer;
+
+
+        public delegate void MassageHandler(string massage);
+        public event MassageHandler MassageEvent;
+
+        public Server(string server, int port)
+        {
+            tcpServer = new TcpListener(IPAddress.Parse(server), port);
+            tcpServer.Start();
+        }
+
+        public void ReadMassage()
+        {
+            try
+            {
+                TcpClient client = tcpServer.AcceptTcpClient();
+                byte[] data = new byte[256];
+                StringBuilder massage = new StringBuilder();
+                NetworkStream stream = client.GetStream();
+
+                do
+                {
+                    int bytes = stream.Read(data, 0, data.Length);
+                    massage.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                }
+                while (stream.DataAvailable);
+
+                stream.Close();
+                client.Close();
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+
+        public void WriteMassage(string massage)
+        {
+            try
+            {
+                TcpClient client = tcpServer.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+                byte[] data = Encoding.UTF8.GetBytes(massage);
+                stream.Write(data, 0, data.Length);
+                stream.Close();
+                client.Close();
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+
+        public void Close()
+        {
+            tcpServer.Stop();
+        }
+    }
+}
