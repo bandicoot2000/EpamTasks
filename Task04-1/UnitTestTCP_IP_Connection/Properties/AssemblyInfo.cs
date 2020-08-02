df9beaf02f39d97b6c5c701beccbe20b8ae711fc
bandicoot2000 <44677038+bandicoot2000@users.noreply.github.com>
2020-08-02 22:52:25 +0300

    Fix.

---
+++ Task04-1/UnitTestTCP_IP_Connection/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestTCP_IP_Connection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestTCP_IP_Connection")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b32bc3a-8a0a-49b2-80d9-2826d966a59e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Task04-1/UnitTestTCP_IP_Connection/TestClientConverter.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TCP_IP_Connection;
+
+namespace UnitTestTCP_IP_Connection
+{
+    [TestClass]
+    public class TestClientConverter
+    {
+        [TestMethod]
+        public void TestConvertMassageToRus_0()
+        {
+            string actual = "asdasdasd";
+            string expected = "асдасдасд";
+            ClientConverter.ConvertMassage(ref actual);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestConvertMassageToRus_1()
+        {
+            string actual = "asda SHHsdasdN";
+            string expected = "асда ЩсдасдН";
+            ClientConverter.ConvertMassage(ref actual);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestConvertMassageToRus_2()
+        {
+            string actual = "asdas фыdasd AAA GGG";
+            string expected = "асдас фыдасд ААА ГГГ";
+            ClientConverter.ConvertMassage(ref actual);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestConvertMassageToEng_0()
+        {
+            string expected = "asdasdasd";
+            string actual = "асдасдасд";
+            ClientConverter.ConvertMassage(ref actual);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestConvertMassageToEng_1()
+        {
+            string expected = "asda SHHsdasdN";
+            string actual = "асда ЩсдасдН";
+            ClientConverter.ConvertMassage(ref actual);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestConvertMassageToEng_2()
+        {
+            string expected = "asdas lldasd AAA GGG";
+            string actual = "асдас llдасд ААА ГГГ";
+            ClientConverter.ConvertMassage(ref actual);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
